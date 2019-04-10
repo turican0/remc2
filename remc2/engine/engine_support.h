dafd147154b783b02714eddace5ac6ca353309f7
turican0 <turican0@gmail.com>
2019-04-10 20:27:40 +0200

    190410-04

--- remc2/engine/engine_support.h
+++ remc2/engine/engine_support.h
@@ -174,7 +174,7 @@ typedef struct _str_0x6E8E {//lenght a8
 	Bit8s byte_0x39_57;//57
 	Bit8u stub4c[1];
 	Bit8s byte_0x3B_59;//59
-	Bit8s byte_0x3C_60_xx;//60
+	Bit8s byte_0x3C_60;//60
 	Bit8s byte_0x3D_61;//61
 	Bit8s byte_0x3E_62;//62 
 	Bit8s byte_0x3F_63;//63 //type
