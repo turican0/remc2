e5b4fa3e4b4102fd608a18b52b4f9f0f0ebc9eda
turican0 <turican0@gmail.com>
2019-08-18 21:12:50 +0200

    190818-02

--- Release/copy-files/copy.bat
+++ Release/copy-files/copy.bat
@@ -4,6 +4,8 @@ copy ..\..\Debug\*.dll ..
 xcopy ..\..\Debug\original-game ..
 copy .\README-install.txt ..
 copy .\config.ini ..
+copy .\run-editor.bat ..
+copy .\run-testlevel.bat ..
 del SDL.dll
 del SDL_mixer.dll
 mkdir ..\original-game
@@ -11,4 +13,5 @@ mkdir ..\original-game\language
 xcopy ..\..\Debug\fix-sound ..\fix-sound  /E /C /I /Q /G /H /R /K /Y /Z /J
 xcopy ..\..\Debug\music-ogg ..\music-ogg  /E /C /I /Q /G /H /R /K /Y /Z /J
 xcopy ..\..\Debug\biggraphics ..\biggraphics  /E /C /I /Q /G /H /R /K /Y /Z /J
+xcopy ..\..\Debug\kiss ..\kiss  /E /C /I /Q /G /H /R /K /Y /Z /J
 copy ..\..\tools\makeczmc2\makeczmc2\l7.txt ..\original-game\language\
