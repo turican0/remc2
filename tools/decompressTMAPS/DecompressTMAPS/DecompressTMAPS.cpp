d63cd08003c6b0f0fe7f3239f670a8ee7c922fc5
turican0 <turican0@gmail.com>
2019-08-26 22:08:37 +0200

    190826-02-2

--- tools/decompressTMAPS/DecompressTMAPS/DecompressTMAPS.cpp
+++ tools/decompressTMAPS/DecompressTMAPS/DecompressTMAPS.cpp
@@ -10,6 +10,11 @@
 #else
 #endif
 
+#define x_BYTE int8
+#define x_WORD int16
+#define x_DWORD int32
+#define x_LONG int32
+
 #define SIZEOF_UNSIGNED_INT 4
 
 #define SIZEOF_UNSIGNED_CHAR 1
@@ -49,6 +54,16 @@ typedef unsigned short  uint16;
 typedef          int    int32;
 typedef   signed int    sint32;
 typedef unsigned int    uint32;
+
+#define _BYTE  uint8
+#define _WORD  uint16
+#define _DWORD uint32
+#define _QWORD uint64
+
+#define LOWORD(x)   (*((_WORD*)&(x)))   // low word
+#define HIWORD(x)   (*((_WORD*)&(x)+1))
+#define LOBYTE(x)   (*((_BYTE*)&(x)))   // low byte
+#define HIBYTE(x)   (*((_BYTE*)&(x)+1))
 
 typedef struct huftable_s {
 	uint32 l1; // +0
@@ -999,6 +1014,381 @@ void write_posistruct_to_bmp(Bit8u* buffer, int width, int height, char* filenam
 	writeImageBMP(filename, width, height, buffer2);
 }
 
+
+__int16 x_WORD_E130C = 0; // weak
+Bit8u* TMAPS00TAB_BEGIN_BUFFER;
+char x_BYTE_E2A20 = 0; // weak
+int x_DWORD_E1304 = 0; // weak
+int x_DWORD_E1308 = 0; // weak
+int x_DWORD_E9C08; // weak
+int x_DWORD_F66F0[504]; // idb//2c76f0
+__int16 x_WORD_E130E = 0; // weak
+__int16 x_WORD_E1310 = 0; // weak
+__int16 x_WORD_E1312ar[2] = { 0,0 }; // weak
+__int16 x_WORD_E1316 = 0; // weak
+
+int sub_724F0(int a1, __int16 a2)
+{
+	int result; // eax
+	int v3; // edx
+
+	HIWORD(v3) = HIWORD(a1);
+	result = *(x_DWORD*)(a1 + 2);
+	LOWORD(v3) = *(x_WORD*)a1;
+	if (!*(x_WORD*)a1)
+		return 0;
+	while (!*(x_DWORD*)(result + 4) || a2 != *(x_WORD*)(result + 26))
+	{
+		--v3;
+		result += 28;
+		if (!(x_WORD)v3)
+			return 0;
+	}
+	return result;
+}
+
+void sub_765FC(__int16 a1, __int16 a2)
+{
+	x_WORD_E130E = a1;
+	x_WORD_E1310 = a2;
+}
+
+void qmemcpy(void* a, void* b, size_t c) {
+	memcpy(a, b, c);
+};
+
+int sub_7677C()
+{
+	int result; // eax
+
+	result = (unsigned __int16)x_WORD_E1312ar[0] - 6;
+	x_DWORD_E1308 += result;
+	return result;
+}
+
+__int16 sub_7678D()//animate sprite
+{
+	x_WORD* v0; // edi
+	x_WORD* v1; // esi
+	__int16 result; // ax
+	x_BYTE* v3; // edi
+	x_BYTE* v4; // esi
+	char v5; // dl
+	x_WORD* v6; // [esp-4h] [ebp-14h]
+	x_WORD* v7; // [esp+0h] [ebp-10h]
+	__int16 i; // [esp+6h] [ebp-Ah]
+	__int16 v9; // [esp+8h] [ebp-8h]
+	int v10; // [esp+Ch] [ebp-4h]
+
+	v10 = (unsigned __int16)x_WORD_E130E;
+	v0 = (x_WORD*)x_DWORD_E1304;
+	v1 = (x_WORD*)(x_DWORD_E1308 + 2);
+	v9 = *(x_WORD*)x_DWORD_E1308;
+	do
+	{
+		while (1)
+		{
+			result = *v1;
+			++v1;
+			v7 = v0;
+			if (result >= 0)
+				goto LABEL_10;
+			if (!(result & 0x4000))
+				break;
+			v0 = (x_WORD*)((char*)v0 + v10 * -result);
+		}
+		v6 = v0;
+		v3 = (char*)v0 + v10 - 1;
+		if (x_BYTE_E2A20 == 1)
+		{
+			if ((x_BYTE)result)
+				* v3 = result;
+		}
+		else
+		{
+			*v3 = result;
+		}
+		v0 = v6;
+	LABEL_10:
+		for (i = result; i; i--)
+		{
+			while (1)
+			{
+				LOBYTE(result) = *(x_BYTE*)v1;
+				v4 = (char*)v1 + 1;
+				result = (unsigned __int8)result;
+				v0 = (x_WORD*)((char*)v0 + (unsigned __int8)result);
+				v5 = *v4;
+				v1 = (x_WORD*)(v4 + 1);
+				if (v5 > 0)
+					break;
+				result = *v1;
+				++v1;
+				do
+				{
+					*v0 = result;
+					++v0;
+					++v5;
+				} while (v5);
+				if (!--i)
+					goto LABEL_18;
+			}
+			do
+			{
+				*v0 = *v1;
+				++v1;
+				++v0;
+				--v5;
+			} while (v5);
+		}
+	LABEL_18:
+		v0 = (x_WORD*)((char*)v7 + v10);
+		--v9;
+	} while (v9);
+	x_DWORD_E1308 = (int)v1;
+	return result;
+}
+
+int sub_76840()
+{
+	x_BYTE* v0; // edi
+	char* v1; // esi
+	__int16 v2; // bx
+	__int16 v3; // cx
+	unsigned __int8 v4; // dl
+	__int16 v5; // ax
+	char v6; // dh
+	char v7; // dh
+	int result; // eax
+	int v9; // [esp-8h] [ebp-Ch]
+	int v10; // [esp-4h] [ebp-8h]
+	__int16 v11; // [esp+2h] [ebp-2h]
+
+	v11 = x_WORD_E130E;
+	v0 = (x_BYTE*)x_DWORD_E1304;
+	v1 = (char*)x_DWORD_E1308;
+	v10 = (unsigned __int16)x_WORD_E1312ar[0] - 6;
+	v9 = x_DWORD_E1308;
+	v2 = x_WORD_E1310;
+	do
+	{
+		++v1;
+		v3 = v11;
+		do
+		{
+			v4 = *v1++;
+			if ((char)v4 > 0)
+			{
+				v3 -= v4;
+			}
+			else
+			{
+				HIBYTE(v5) = -1;
+				LOBYTE(v5) = v4;
+				v3 += v5;
+			}
+			if ((char)v4 > 0)
+			{
+				v7 = *v1++;
+				do
+				{
+					if (x_BYTE_E2A20 == 1)
+					{
+						if (v7)
+							* v0 = v7;
+					}
+					else
+					{
+						*v0 = v7;
+					}
+					++v0;
+					--v4;
+				} while (v4);
+			}
+			else
+			{
+				do
+				{
+					v6 = *v1++;
+					if (x_BYTE_E2A20 == 1)
+					{
+						if (v6)
+							* v0 = v6;
+					}
+					else
+					{
+						*v0 = v6;
+					}
+					++v0;
+					++v4;
+				} while (v4);
+			}
+		} while (v3);
+		--v2;
+	} while (v2);
+	result = v10;
+	x_DWORD_E1308 = v10 + v9;
+	return result;
+}
+
+void sub_766A4()//animate sprite
+{
+	__int16 result; // ax
+
+	qmemcpy(&x_WORD_E1316, (void*)x_DWORD_E1308, sizeof(x_WORD_E1316));
+	x_DWORD_E1308 += 2;
+	x_DWORD_E1308 += 8;
+	while (x_WORD_E1316)
+	{
+		--x_WORD_E1316;
+		qmemcpy(x_WORD_E1312ar, (void*)x_DWORD_E1308, 4u);
+		x_DWORD_E1308 += 4;
+		qmemcpy(&x_WORD_E130C, (void*)x_DWORD_E1308, sizeof(x_WORD_E130C));
+		x_DWORD_E1308 += 2;
+		switch (x_WORD_E130C)
+		{
+		case 7:
+			result = sub_7678D();
+			break;
+		case 4:
+			result = sub_7677C();
+			break;
+		case 15:
+			result = sub_76840();
+			break;
+		default:
+			result = sub_7677C();
+			break;
+		}
+	}
+	//return result;
+}
+
+__int16 sub_76752()
+{
+	__int16 result; // ax
+
+	x_WORD_E130E = *(x_WORD*)(x_DWORD_E1308 + 2);
+	result = *(x_WORD*)(x_DWORD_E1308 + 4);
+	x_WORD_E1310 = *(x_WORD*)(x_DWORD_E1308 + 4);
+	x_DWORD_E1308 += 6;
+	return result;
+}
+
+int sub_76619(int a1, int a2)//animate sprite
+{
+	x_DWORD_E1308 = a1;
+	x_DWORD_E1304 = a2;
+	while (1)
+	{
+		qmemcpy(x_WORD_E1312ar, (void*)x_DWORD_E1308, 4u);
+		x_DWORD_E1308 += 4;
+		qmemcpy(&x_WORD_E130C, (void*)x_DWORD_E1308, sizeof(x_WORD_E130C));
+		x_DWORD_E1308 += 2;
+		if (x_WORD_E130C != -20718)
+			break;
+		sub_76752();
+	}
+	if (x_WORD_E130C != -3590)
+		return 0;
+	sub_766A4();
+	return x_DWORD_E1308;
+}
+
+int sub_72350(int a1)//253350 //animates sprite
+{
+	int v1; // esi
+	int v2; // edi
+	int v3; // eax
+	__int16 v4; // cx
+	int result; // eax
+
+	if (*(x_WORD*)(a1 + 22) > * (x_WORD*)(a1 + 16))
+	{
+		*(x_WORD*)(a1 + 22) = 1;
+		*(x_DWORD*)(a1 + 8) = *(unsigned __int16*)(a1 + 14);
+	}
+	v1 = **(x_DWORD * *)(a1 + 4);
+	v2 = *(x_DWORD*)(a1 + 8);
+	sub_765FC(*(x_WORD*)(a1 + 18), 0);
+	v1 += 6;
+	v3 = sub_76619(v1 + v2, v1);
+	v4 = *(x_WORD*)(a1 + 22);
+	result = v3 - v1;
+	*(x_DWORD*)(a1 + 8) = result;
+	*(x_WORD*)(a1 + 22) = v4 + 1;
+	return result;
+}
+
+
+
+int sub_715B0()//2525b0
+{
+	unsigned __int16 v0; // bx
+	int result; // eax
+	int* v2; // ecx
+	char v3; // cl
+	char v4; // cl
+	int v5; // eax
+	__int16 v6; // si
+	unsigned __int16 i; // bx
+	int* v8; // edi
+	char v9; // cl
+	int v10; // eax
+
+	v0 = 0;
+	do
+	{
+		result = 4 * v0;
+		v2 = *(int**)((char*)x_DWORD_F66F0 + result);
+		if (v2)
+		{
+			result = *v2;
+			v3 = *(x_BYTE*)* v2;
+			if (v3 & 8)
+			{
+				if (v3 & 0x20)
+				{
+					result = 5 * v0;
+					v6 = *(x_WORD*)(TMAPS00TAB_BEGIN_BUFFER + 10 * v0 + 8);
+					for (i = *(x_WORD*)(TMAPS00TAB_BEGIN_BUFFER + 10 * v0 + 8); i < 0x1F8u; i++)
+					{
+						result = 5 * i;
+						if (v6 != *(x_WORD*)(TMAPS00TAB_BEGIN_BUFFER + 10 * i + 8))
+							break;
+						result = 4 * i;
+						v8 = (int*)x_DWORD_F66F0[i];
+						if (v8)
+						{
+							result = *v8;
+							v9 = *(x_BYTE*)* v8 & 0xF7;
+							*(x_BYTE*)* v8 = v9;
+							if (v9 & 1)
+							{
+								v10 = sub_724F0(x_DWORD_E9C08, i);
+								result = sub_72350(v10);
+							}
+						}
+					}
+					v0 = i - 1;
+				}
+				else
+				{
+					v4 = v3 & 0xF7;
+					*(x_BYTE*)result = v4;
+					if (v4 & 1)
+					{
+						v5 = sub_724F0(x_DWORD_E9C08, v0);
+						result = sub_72350(v5);
+					}
+				}
+			}
+		}
+		v0++;
+	} while (v0 < 0x1F8u);
+	return result;
+}
+
 int main()
 {
 	FILE* fptrTMAPSdata;
@@ -1048,6 +1438,8 @@ int main()
 
 		int width = *(Bit16u*)&buffer[2];
 		int height = *(Bit16u*)&buffer[4];
+
+		int sub_715B0();
 
 		FILE* fptw2;
 		char filenamedata[300];
