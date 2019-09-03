ddc43583d1d6d39e9bea2a7c8621fc5e8e9792b7
turican0 <turican0@gmail.com>
2019-09-03 19:44:40 +0200

    190903-01

---
+++ tools/makeczmc2/makeczmc2/copycz.bat
@@ -0,0 +1,9 @@
+makeczmc2
+copy l7.txt ..\..\..\..\Magic2\mc2-orig\language\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig\NETHERW\LANGUAGE\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-c2\language\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-c2\NETHERW\LANGUAGE\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-copy\language\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-copy\NETHERW\LANGUAGE\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-copy2\language\
+copy l7.txt ..\..\..\..\Magic2\mc2-orig-copy2\NETHERW\LANGUAGE\
