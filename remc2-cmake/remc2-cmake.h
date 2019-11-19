17bfc51125b733073cfd53171709da666afe5d8b
Tomas Vesely <tomas.vesely@jansen-display.com>
2019-11-19 11:28:30 +0100

    191119-02

--- remc2-cmake/remc2-cmake.h
+++
@@ -1,8 +0,0 @@
-﻿// remc2-cmake.h: Soubor k zahrnutí pro standardní systémové soubory k zahrnutí
-// nebo soubory k zahrnutí specifické pro projekt.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Zde odkažte na dodatečné hlavičky, které program vyžaduje.
