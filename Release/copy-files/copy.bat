b6bed29facdc5f8c264b6ecbc8731ae20c70a816
Tomas Vesely <tomas.vesely@jansen-display.com>
2019-11-22 09:09:13 +0100

    191122-02

--- Release/copy-files/copy.bat
+++ Release/copy-files/copy.bat
@@ -8,8 +8,11 @@ copy .\README-install.txt ..
 copy .\config.ini ..
 copy .\run-editor.bat ..
 copy .\run-testlevel.bat ..
+copy .\extract-GOG-CD.bat ..
+copy .\dosbox-copy.conf ..
 del SDL.dll
 del SDL_mixer.dll
+mkdir ..\original-game-GOG
 mkdir ..\original-game
 mkdir ..\original-game\language
 xcopy ..\..\Debug\fix-sound ..\fix-sound  /E /C /I /Q /G /H /R /K /Y /Z /J
@@ -17,4 +20,11 @@ xcopy ..\..\Debug\music-ogg ..\music-ogg  /E /C /I /Q /G /H /R /K /Y /Z /J
 xcopy ..\..\Debug\biggraphics ..\biggraphics  /E /C /I /Q /G /H /R /K /Y /Z /J
 xcopy ..\..\Debug\font ..\font  /E /C /I /Q /G /H /R /K /Y /Z /J
 xcopy ..\..\Debug\kiss ..\kiss  /E /C /I /Q /G /H /R /K /Y /Z /J
+copy .\lang\l1.txt ..\original-game\language\
+copy .\lang\l2.txt ..\original-game\language\
+copy .\lang\l3.txt ..\original-game\language\
+copy .\lang\l4.txt ..\original-game\language\
+copy .\lang\l5.txt ..\original-game\language\
+copy .\lang\l6.txt ..\original-game\language\
+copy .\lang\d2.txt ..\original-game\language\
 copy ..\..\tools\makeczmc2\makeczmc2\l7.txt ..\original-game\language\
---
+++ Release/copy-files/extract-GOG-CD.bat
@@ -0,0 +1,2 @@
+..\original-game-GOG\dosbox.exe .\dosbox-copy.conf
+..\original-game-GOG\DOSBOX\dosbox.exe -conf ".\dosbox-copy.conf" -c exit
