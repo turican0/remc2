d0f02dd3776bce00323926b427d816542fdb7a7e
Timothy Hobbs <thobbsinteractive@gmail.com>
2021-02-06 18:56:32 +0100

    refactor: removed config.h file

--- remc2/portability/mctypes.h
+++ remc2/portability/mctypes.h
@@ -5,27 +5,18 @@
 #include <string>
 #include <cstdint>
 
+#if defined(WIN32)
+# pragma warning(disable:4996)
+#endif
+
 #ifdef USE_DOSBOX
 #include "logging.h"
 #include "dos_system.h"
-#else
-#include "../dosbox_files/config.h"
 #endif //USE_DOSBOX
 
-typedef          char   int8;
-typedef   signed char   sint8;
-typedef unsigned char   uint8;
-typedef          short  int16;
-typedef   signed short  sint16;
-typedef unsigned short  uint16;
-typedef          int    int32;
-typedef   signed int    sint32;
-typedef unsigned int    uint32;
-
-#define x_BYTE int8
-#define x_WORD int16
-#define x_DWORD int32
-#define x_LONG int32
+#define x_BYTE char
+#define x_WORD short
+#define x_DWORD int
 
 typedef struct {
 	uint8_t* data;
