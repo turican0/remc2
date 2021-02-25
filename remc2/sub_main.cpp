9ac0ecd901e2412aece7c6eb28d5d5de8069d9fe
Tomas Vesely <turican0@gmail.com>
2021-02-25 08:28:10 +0100

    change config_LOAD_EDITED_LEVEL to false

--- remc2/sub_main.cpp
+++ remc2/sub_main.cpp
@@ -76,7 +76,7 @@ int count_begin = 1;//1
 //int debugnextlevel = 0;
 
 bool config_EDITOR = false;
-bool config_LOAD_EDITED_LEVEL = true;
+bool config_LOAD_EDITED_LEVEL = false;
 #define EDITOR
 #define LOAD_EDITED_LEVEL
 
