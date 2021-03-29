1fd69f245cd62163331c32dde8a232dcc7b799c0
Tomas Vesely <turican0@gmail.com>
2021-03-29 10:35:30 +0200

    210329-03

--- Debug/testRegressions_wxcopy.bat
+++ Debug/testRegressions_wxcopy.bat
@@ -19,4 +19,94 @@ echo %ERRORLEVEL%
 IF %ERRORLEVEL% NEQ 20 echo "test level2 failed"
 IF %ERRORLEVEL% EQU 20 echo "test level2 ok"
 
+echo "testing level3"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level3\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 2
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level3 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level3 ok"
+
+echo "testing level4"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level4\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 3
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level4 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level4 ok"
+
+echo "testing level5"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level5\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 4
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level5 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level5 ok"
+
+echo "testing level6"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level6\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 5
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level6 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level6 ok"
+
+echo "testing level7"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level7\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 6
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level7 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level7 ok"
+
+echo "testing level8"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level8\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 7
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level8 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level8 ok"
+
+echo "testing level9"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level9\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 8
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level9 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level9 ok"
+
+echo "testing level10"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level10\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 9
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level10 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level10 ok"
+
+echo "testing level11"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level11\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 10
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level11 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level11 ok"
+
+echo "testing level12"
+del /q ..\remc2\memimages\regressions\*.*
+copy ..\remc2\memimages\regressions\level12\*.* ..\remc2\memimages\regressions\
+remc2_without_xcopy.exe reglevel 11
+echo ERRORLEVEL 
+echo %ERRORLEVEL%
+IF %ERRORLEVEL% NEQ 20 echo "test level12 failed"
+IF %ERRORLEVEL% EQU 20 echo "test level12 ok"
+
 set /p DUMMY=Hit ENTER to continue...
