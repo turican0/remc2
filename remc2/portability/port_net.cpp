82f0b1a89f0a6a97561c50e34c10bda846abec6a
Tomas Vesely <turican0@gmail.com>
2021-04-21 08:47:11 +0200

    210421-02

--- remc2/portability/port_net.cpp
+++ remc2/portability/port_net.cpp
@@ -1,8 +1,6 @@
 #include "port_net.h"
 
-#include "include/cppnet.h"
-#include "include/cppnet_socket.h" 
-
+/*
 int sockInit(void)
 {
 #ifdef _WIN32
@@ -28,7 +26,8 @@ int sockClose(SOCKET sock)
 	int status = 0;
 
 #ifdef _WIN32
-	status = shutdown(sock, 2/*SD_BOTH*/);
+	//status = shutdown(sock, SD_BOTH);
+	status = shutdown(sock, 2);
 	if (status == 0) { status = closesocket(sock); }
 #else
 	status = shutdown(sock, SHUT_RDWR);
@@ -38,4 +37,55 @@ int sockClose(SOCKET sock)
 	return status;
 
 }
+*/
 
+using namespace cppnet;
+
+#ifdef _WIN32 || _WIN64
+#include <winsock2.h>
+void SetNoDelay(const uint64_t& sock) {
+	int opt = 1;
+	int ret = setsockopt(sock, SOL_SOCKET, TCP_NODELAY, (const char*)&opt, sizeof(opt));
+}
+#else
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+void SetNoDelay(const uint64_t& sock) {
+	int optval = 1;
+	setsockopt(sock, IPPROTO_TCP, TCP_NODELAY,
+		&optval, static_cast<socklen_t>(sizeof(optval)));
+}
+#endif
+
+static std::atomic_int count;
+
+void OnConnection(const Handle& handle, uint32_t error) {
+
+	count++;
+	if (error == CEC_SUCCESS) {
+		std::cout << " accept a socket. count: " << count << std::endl;
+		//SetNoDelay(handle);
+	}
+}
+
+void OnMessage(const Handle& handle, std::shared_ptr<cppnet::Buffer> data, uint32_t) {
+	char buff[65535];
+
+	while (data->GetCanReadLength()) {
+		int ret = data->Read(buff, 65535);
+		handle->Write(buff, ret);
+	}
+}
+
+void NetworkTestServer()
+{
+	cppnet::CppNet net;
+	net.Init(4);
+
+	net.SetAcceptCallback(OnConnection);
+	net.SetReadCallback(OnMessage);
+
+	net.ListenAndAccept("0.0.0.0", 8921);
+
+	net.Join();
+}
--- remc2/portability/port_net.h
+++ remc2/portability/port_net.h
@@ -1,24 +1,11 @@
 #ifndef PORT_NETWORK
 #define PORT_NETWORK
 
+#include <atomic>
+#include <iostream>
 
-#ifdef _WIN32
-  /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501  /* Windows XP. */
-#endif
-#include <windows.h>
-//#include <winsock2.h>
-//#include <Ws2tcpip.h>
-#pragma comment(lib, "Ws2_32.lib")
-#else
-  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
-#include <unistd.h> /* Needed for close() */
-#endif
+#include "include/cppnet.h"
+#include "include/cppnet_socket.h" 
 
-int sockInit(void);
 
 #endif //PORT_TIME
