9230f9192fefd11423b33481ceca9f6afa2d4d9e
Timothy Hobbs <thobbsinteractive@gmail.com>
2021-04-22 21:39:01 +0200

    feat: definition of ViewPort type

---
+++ remc2/engine/ViewPort.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+
+#include <cstdint>
+
+struct ViewPort
+{
+	uint16_t PosX = 0;
+	uint16_t PosY = 0;
+	uint16_t Width = 0;
+	uint16_t Height = 0;
+
+	ViewPort()
+	{
+		PosX = 0;
+		PosY = 0;
+		Width = 0;
+		Height = 0;
+	};
+
+	ViewPort(uint16_t viewPortPosX, uint16_t viewPortPosY, uint16_t viewPortWidth, uint16_t viewPortHeight)
+	{
+		PosX = viewPortPosX;
+		PosY = viewPortPosY;
+		Width = viewPortWidth;
+		Height = viewPortHeight;
+	};
+};
+
+#endif //VIEWPORT_H
