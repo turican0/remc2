7ca5facc3cdbb7fc8daed3cbf2da3d28e6075d58
Timothy Hobbs <thobbsinteractive@gmail.com>
2021-04-24 14:54:41 +0200

    refactor: moved sub_58490_radix_3d_2, sub_58210_radix_tan, sub_72633_maybe_tan and sub_581E0_maybe_tan2 to Maths

--- remc2/utilities/Maths.cpp
+++ remc2/utilities/Maths.cpp
@@ -1,5 +1,12 @@
 #include "Maths.h"
 
+unsigned int Maths::sub_58490_radix_3d_2(axis_3d* a1, axis_3d* a2)//239490
+{
+	return Maths::sub_7277A_radix_3d(
+		(signed __int16)(a2->x - a1->x) * (signed __int16)(a2->x - a1->x)
+		+ (signed __int16)(a2->y - a1->y) * (signed __int16)(a2->y - a1->y));
+}
+
 unsigned int Maths::sub_7277A_radix_3d(unsigned int a1)//25377a
 {
 	uint32_t v1; // eax
@@ -11,6 +18,85 @@ unsigned int Maths::sub_7277A_radix_3d(unsigned int a1)//25377a
 	for (i = (unsigned __int16)x_WORD_727B0[v1]; (signed int)(a1 / i) < (signed int)i; i = (a1 / i + i) >> 1)
 		;
 	return i;
+}
+
+int16_t Maths::sub_58210_radix_tan(axis_3d* a1, axis_3d* a2)//239210
+{
+	int16_t v2; // ax
+	v2 = sub_58490_radix_3d_2(a1, a2);//
+	return sub_72633_maybe_tan(a1->z - a2->z, -v2);
+}
+
+uint16_t Maths::sub_72633_maybe_tan(int16_t a1, int16_t a2)//253633 //4c //3e
+{
+	//signed int v2; // ebx
+	uint16_t result; // ax
+	//int v4; // ebx
+	//int v5; // eax
+	//int v6; // ebx
+
+	//debug
+	//loadfromsnapshot((char*)"0160-00253633", (uint8_t*)&a1, 0x354ef8, 2);//4c
+	//loadfromsnapshot((char*)"0160-00253633", (uint8_t*)&a2, 0x354efc, 2);//3e
+	//loadfromsnapshot((char*)"0160-00253633", (uint8_t*)x_WORD_DE350, 0x2af350, 600);//fix it!
+	//debug
+
+	if (!a1)
+	{
+		if (!a2)
+			return 0;
+	}
+	if (a1 < 0)
+	{
+		if (a2 < 0)
+		{
+			if (-a1 < -a2)
+				result = 0x800 - x_WORD_DE350[(-a1 << 8) / -a2];
+			else
+				result = x_WORD_DE350[(-a2 << 8) / -a1] + 0x600;
+		}
+		else if (-a1 < a2)
+		{
+			result = x_WORD_DE350[(-a1 << 8) / a2] + 0x400;
+		}
+		else
+		{
+			result = 0x600 - x_WORD_DE350[(a2 << 8) / -a1];
+		}
+	}
+	else if (a2 < 0)
+	{
+		if (a1 < -a2)
+			result = x_WORD_DE350[(a1 << 8) / -a2];
+		else
+			result = 0x200 - x_WORD_DE350[(-a2 << 8) / a1];
+	}
+	else if (a1 < a2)
+	{
+		result = 0x400 - x_WORD_DE350[(a1 << 8) / a2];
+	}
+	else
+	{
+		result = x_WORD_DE350[(a2 << 8) / a1] + 0x200;
+	}
+	return result;
+}
+
+int16_t Maths::sub_581E0_maybe_tan2(axis_3d* a1, axis_3d* a2)//2391E0
+{
+	//354f10->35f418 +2->35012a .. 012a
+	//354f14->354f20 +2->350168 .. 0168
+	//0168-18a=3e
+	//354f10->35f418->ec0141 .. 0141
+	//354f14->354f20->012a018d .. 018d
+	//18d-141=4c
+
+	//ebx=a1 //141
+	//edx=a2 //1d8
+	//ax edx+2
+
+	return sub_72633_maybe_tan(a2->x - a1->x, a2->y - a1->y);//253633
+	//rozdil1- 4c rozdil2 - 3e
 }
 
 void Maths::x_BitScanReverse(uint32_t* Destination, uint32_t Source) 
