f6627410ea0eb3a91c7a7d36e5e230257d3b8ebb
Tomas Vesely <turican0@gmail.com>
2022-06-23 10:25:27 +0200

    renames in Amimation.h

--- remc2/engine/Animation.h
+++ remc2/engine/Animation.h
@@ -25,29 +25,29 @@ extern TColor* x_DWORD_E12F4x; // weak
 extern int16_t x_WORD_17DB48; // weak
 extern char x_BYTE_D41C0; // weak
 
-void PlayInfoFmv(__int16 a1, __int16 a2, Type_SoundEvent_E17CC* a3x, char* path);//257160
+void PlayInfoFmv(__int16 a1, __int16 a2, Type_SoundEvent_E17CC* pSoundEvent, char* path);//257160
 void FlvInitSet_473B0();
-void PlayIntoSoundEvents_1B280(Type_SoundEvent_E17CC* a1x);
+void PlayIntoSoundEvents_1B280(Type_SoundEvent_E17CC* pSoundEvent);
 void sub_75DB0();
 void sub_75E70();
 
 void sub_loc_1B2E6();
-void sub_loc_1B2F7(Type_SoundEvent_E17CC* a2x);
+void sub_loc_1B2F7(Type_SoundEvent_E17CC* pSoundEvent);
 void sub_loc_1B316(uint8_t a1);
-void sub_loc_1B334(Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B352(Type_SoundEvent_E17CC* a2x);
+void sub_loc_1B334(Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B352(Type_SoundEvent_E17CC* pSoundEvent);
 void sub_loc_1B36C();
-void sub_loc_1B37D(Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B398(Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B3C9(Type_SoundEvent_E17CC* a1x);
+void sub_loc_1B37D(Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B398(Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B3C9(Type_SoundEvent_E17CC* pSoundEvent);
 void sub_loc_1B413();
-void sub_loc_1B424(uint16_t a1, Type_SoundEvent_E17CC* a2x);
+void sub_loc_1B424(uint16_t a1, Type_SoundEvent_E17CC* pSoundEvent);
 void sub_loc_1B450();
-void sub_loc_1B461(uint16_t a1, Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B4A0(uint16_t a1, Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B4DF(uint16_t a1, Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B51E(Type_SoundEvent_E17CC* a2x);
-void sub_loc_1B54A(uint16_t a1, Type_SoundEvent_E17CC* a2x);
+void sub_loc_1B461(uint16_t a1, Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B4A0(uint16_t a1, Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B4DF(uint16_t a1, Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B51E(Type_SoundEvent_E17CC* pSoundEvent);
+void sub_loc_1B54A(uint16_t a1, Type_SoundEvent_E17CC* pSoundEvent);
 void sub_loc_1B589();
 void sub_loc_1B5A7();
 void sub_loc_1B5BC(uint16_t a1);
