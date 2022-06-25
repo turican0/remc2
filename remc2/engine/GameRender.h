a89244c5c86cf1a92f8867bb7fe34859e617cdb0
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-06-25 17:32:52 +0200

    refactor: renamed SetRenderViewPortSize to SetRenderViewPortSize_40C50

--- remc2/engine/GameRender.h
+++ remc2/engine/GameRender.h
@@ -118,7 +118,7 @@ public:
 	void SetTextures(std::array<uint8_t*, 256> &textureAdresses);
 	void SetX_BYTE_F6EE0_tablesx(uint8_t pX_BYTE_F6EE0_tablesx[]);
 	void SetRenderViewPortSize_BCD45(ViewPort viewPort, uint16_t screenWidth, uint16_t screenHeight);
-	void SetRenderViewPortSize_BCD45(uint8_t viewPortSizeSetting, uint16_t screenWidth, uint16_t screenHeight);
+	void SetRenderViewPortSize_40C50(uint8_t viewPortSizeSetting, uint16_t screenWidth, uint16_t screenHeight);
 	void SetRenderViewPortSize_BCD45(uint8_t* ptrScreenBufferStart, uint16_t viewPortWidth, uint16_t viewPortHeight, uint16_t screenWidth);
 	void SetRenderThreads(uint8_t renderThreads);
 	uint8_t GetRenderThreads();
