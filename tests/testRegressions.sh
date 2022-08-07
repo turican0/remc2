cdd4a9a3a661c475eeb1e07707e1f937831e21a0
Sebastian Birk <sebastian.birk@gmail.com>
2022-08-08 01:08:59 +0200

    Pass memimages_path to regression test loop on Linux

--- tests/testRegressions.sh
+++ tests/testRegressions.sh
@@ -7,7 +7,8 @@ remc2_executable=./build/Debug/inst/bin/remc2
 data_location=~/.local/share/remc2/regression_tests
 
 # !NOTE!: the regression files have to be exactly at ../memimages/regressions relative to the remc2 executable
-regression_files=./build/Debug/inst/memimages/regressions/
+memimages_path=$(realpath ./build/Debug/inst/memimages/)
+regression_files=$memimages_path/regressions/
 
 test_level() {
     level=$1
@@ -16,12 +17,14 @@ test_level() {
     rm -f "${regression_files}/*"
     cp "${data_location}/level${level}/"* "${regression_files}"
     gamelevel="$(($level-1))"
-    ./build/Debug/inst/bin/remc2 --mode_test_regressions_game reglevel $gamelevel 2>&1 | grep 'Regression compare sequence error'
+    pushd ./build/Debug/inst/bin > /dev/null
+    ./remc2 reglevel $gamelevel --mode_test_regressions_game --memimages_path $memimages_path 2>&1 | grep 'Regression compare sequence error'
     if [ ${PIPESTATUS[0]} -eq 20 ]; then
         echo "test level$level ok"
     else
         echo "test level$level failed"
     fi
+    popd > /dev/null
 }
 
 for i in `seq 1 25`; do
