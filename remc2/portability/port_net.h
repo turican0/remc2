2453334ac12a6cba4678f79bdcc4c405de25cdf7
turican0 <41465132+turican0@users.noreply.github.com>
2022-08-11 17:09:22 +0200

    update: port.cpp/port.h

--- remc2/portability/port_net.h
+++ remc2/portability/port_net.h
@@ -38,6 +38,24 @@ typedef struct {//lenght 66(changed to 70)
 	//uint8_t ncb_reserved_50[16];// not used
 }
 myNCB;
+
+typedef struct {//lenght 66(changed to 70)
+	uint8_t ncb_command_0;
+	uint8_t ncb_retcode_1;
+	uint8_t ncb_lsn_2;
+	uint8_t ncb_num_3;
+	uint32_t ncb_buffer_4;
+	uint16_t ncb_bufferLength_8;
+	char ncb_callName_10[16];
+	char ncb_name_26[16];
+	uint8_t ncb_rto_42;
+	uint8_t ncb_sto_43;
+	//p64align ncb_post_44;// not used
+	//uint8_t ncb_lana_num_48;// not used
+	uint8_t ncb_cmd_cplt_49;//lock for end command
+	//uint8_t ncb_reserved_50[16];// not used
+}
+shadow_myNCB;
 #pragma pack (16)
 
 void makeNetwork(myNCB* connection);
@@ -47,7 +65,7 @@ void EndMyNetLib();
 void AddRecMess(std::string message);
 std::string GetRecMess();
 unsigned int GetRecCount();
-void CleanMessages(myNCB* locNCB);
+void CleanMessages(myNCB locNCB);
 
 void printState(myNCB** connections);
 void printState2(char* text);
@@ -57,5 +75,7 @@ bool ReceiveServerAddName();
 void InitMyNetLib(bool iam_server, char* ip, int networkPort, int serverPort);
 
 void debug_net_printf(const char* format, ...);
+
+void timeState(bool start, char* text);
 
 #endif //PORT_NETWORK
