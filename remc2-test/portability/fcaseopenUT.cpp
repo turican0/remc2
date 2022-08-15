e51590a7f09e687a636a6e421eb5f5551a9c6913
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-08-16 01:12:28 +0200

    feat: implemented remc2-test project

---
+++ remc2-test/portability/fcaseopenUT.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "../pch.h"
+#include "../../remc2/portability/fcaseopen.h"
+TEST (fcaseopen, tokens) {
+#ifdef __linux__
+    std::string path1 = "/asdf/dummy/file.c";
+    std::string path2 = "./dummy2/file2.c";
+
+    auto tokens1 = GetTokensFromPath(path1);
+    auto tokens2 = GetTokensFromPath(path2);
+
+    EXPECT_EQ ("asdf", tokens1[0]);
+    EXPECT_EQ ("dummy", tokens1[1]);
+    EXPECT_EQ ("file.c", tokens1[2]);
+    EXPECT_EQ (".", tokens2[0]);
+    EXPECT_EQ ("dummy2", tokens2[1]);
+    EXPECT_EQ ("file2.c", tokens2[2]);
+#else //__linux__
+    EXPECT_EQ(1, 1);
+#endif //__linux__
+}
+
+TEST (fcaseopen, casepath) {
+#ifdef __linux__
+    EXPECT_EQ(casepath("/usr/share"), "/usr/share");
+    EXPECT_EQ(casepath("/usr/share/"), "/usr/share/");
+    EXPECT_EQ(casepath("/usr/../usr/share"), "/usr/../usr/share");
+    EXPECT_EQ(casepath("/USR/SHARE"), "/usr/share");
+    EXPECT_EQ(casepath("remc2unittests"), "remc2unittests");
+    EXPECT_EQ(casepath("./remc2unittests"), "./remc2unittests");
+    EXPECT_EQ(casepath("./REMC2unittests"), "./remc2unittests");
+    EXPECT_EQ(casepath("./not_existing_file"), "./not_existing_file");
+#else //__linux__
+    EXPECT_EQ(1, 1);
+#endif //__linux__
+}
