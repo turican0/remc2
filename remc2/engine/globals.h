308ac1065e75bf7da5623665c19532813b408ac5
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-08-28 12:42:30 +0200

    fix: added globals to VS project

--- remc2/engine/globals.h
+++ remc2/engine/globals.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include <cstdint>
+#ifndef GLOBALS_H
+#define GLOBALS_H
 
+#include <cstdint>
 #include "Type_F2C20ar.h"
 
 extern uint8_t* pdwScreenBuffer_351628;
-extern __int16_t x_WORD_180660_VGA_type_resolution;
+extern int16_t x_WORD_180660_VGA_type_resolution;
 extern type_F2C20ar str_F2C20ar;
 extern uint32_t screenHeight_180624;
 extern uint32_t screenWidth_18062C;
 extern int iScreenWidth_DE560;
 extern uint8_t* ViewPortRenderBufferStart_DE558;
 extern uint8_t* ViewPortRenderBufferAltStart_DE554;
+
+#endif //GLOBALS_H
