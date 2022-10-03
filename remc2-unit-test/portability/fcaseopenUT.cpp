c09ae2593acfed488d32195405b1048722c6d176
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-10-03 19:51:58 +0200

    fix: fixed paths in Linux fcaseopenUT test

--- remc2-unit-test/portability/fcaseopenUT.cpp
+++ remc2-unit-test/portability/fcaseopenUT.cpp
@@ -26,9 +26,9 @@ TEST (fcaseopen, casepath) {
     ASSERT_EQ(casepath("/usr/share/"), "/usr/share/");
     ASSERT_EQ(casepath("/usr/../usr/share"), "/usr/../usr/share");
     ASSERT_EQ(casepath("/USR/SHARE"), "/usr/share");
-    ASSERT_EQ(casepath("remc2-test"), "remc2-test");
-    ASSERT_EQ(casepath("./remc2-test"), "./remc2-test");
-    ASSERT_EQ(casepath("./REMC2-test"), "./remc2-test");
+    ASSERT_EQ(casepath("remc2-unit-test"), "remc2-unit-test");
+    ASSERT_EQ(casepath("./remc2-unit-test"), "./remc2-unit-test");
+    ASSERT_EQ(casepath("./REMC2-unit-test"), "./remc2-unit-test");
     ASSERT_EQ(casepath("./not_existing_file"), "./not_existing_file");
 #else //__linux__
     ASSERT_EQ(1, 1);
