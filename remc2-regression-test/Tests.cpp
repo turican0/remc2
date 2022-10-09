f0e5aaa254dba53896218b392b7914ad19aed8ad
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-10-09 18:39:45 +0200

    feat: added count of failed tests

--- remc2-regression-test/Tests.cpp
+++ remc2-regression-test/Tests.cpp
@@ -8,14 +8,26 @@
 int main(int argc, char** argv)
 {
 	//if (CommandLineParams.DoShowDebugMessages1()) -- for suppress messages
+	int numFailedTests = 0;
 
 	printf("--- Regressions tests ---\n");
-	for(int i = 1 ; i <= 25; i++)
-		if(i!=22)
-			run_regtest(i);
+	for (int i = 1; i <= 25; i++)
+		if (i != 22)
+			if (run_regtest(i) != 0)
+			{
+				numFailedTests++;
+			}
+
 	printf("\n");
 
-	printf("\nAll tests passed\n");
+	if (numFailedTests == 0)
+	{
+		printf("\nAll tests passed\n");
+	}
+	else
+	{
+		printf("\n%d tests failed!\n", numFailedTests);
+	}
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 	return(0);
