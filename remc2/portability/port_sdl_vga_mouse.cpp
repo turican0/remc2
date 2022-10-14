4677b6689f4659e359b78022a83aefdbcc717d60
Tomas Vesely <turican0@gmail.com>
2022-10-14 10:13:02 +0200

    fix: no use new framebuffer when change palette

--- remc2/portability/port_sdl_vga_mouse.cpp
+++ remc2/portability/port_sdl_vga_mouse.cpp
@@ -836,8 +836,8 @@ void VGA_Blit(Uint8* srcBuffer) {
 			return;
 		}
 	}
-
-	memcpy(m_gamePalletisedSurface->pixels, srcBuffer, m_gamePalletisedSurface->h * m_gamePalletisedSurface->w);
+	if (srcBuffer)
+		memcpy(m_gamePalletisedSurface->pixels, srcBuffer, m_gamePalletisedSurface->h * m_gamePalletisedSurface->w);
 	
 	if (SDL_MUSTLOCK(m_gamePalletisedSurface)) {
 		SDL_UnlockSurface(m_gamePalletisedSurface);
