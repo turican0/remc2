ffb2cb4377fa45f396a251a7b8a96f6505cecb9f
Timothy Hobbs <thobbsinteractive@gmail.com>
2022-11-10 21:13:16 +0100

    feat: added new loggingLevel parameter to config file

--- remc2/engine/read_config.h
+++ remc2/engine/read_config.h
@@ -31,6 +31,7 @@ extern int texturepixels;
 extern int maxGameFps;
 extern int fmvFps;
 extern int menuFps;
+extern std::string loggingLevel;
 extern int displayIndex;
 extern int windowResWidth;
 extern int windowResHeight;
