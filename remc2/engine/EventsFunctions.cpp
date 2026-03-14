f061bdf1da00a3f02dd33b80b4376f4675ba29ac
Tomáš Veselý <turican0@gmail.com>
2026-03-14 09:12:45 +0100

    fix test for change res after hidden level

--- remc2/engine/EventsFunctions.cpp
+++ remc2/engine/EventsFunctions.cpp
@@ -31613,7 +31613,7 @@ void InGameLoop_47320()//228320
 	D41A0_0.array_0x2BDE[D41A0_0.LevelIndex_0xc].dw_w_b_0_2BDE_11230.word[1] = 0;
 
 	//fix res on begin level for hidden levels-neoriginal code
-	if (!IsDefaultResolution(gameResWidth, gameResHeight))
+	if (!(gameResWidth == 320 && gameResHeight == 200))
 	{
 		VGA_Resize(320, 200);
 		screenWidth_18062C = 320;
